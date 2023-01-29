bd72664b2d9f8000b298d30bcf26e9d7cc2e54a5
unknown <arjunrathaur@hotmail.com>
2023-01-28 22:53:07 -0500

    Initial commit (/test created)

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Test from './components/test.js';
+
+function App() {
+  return (
+    <div>
+      <h1>hello world!</h1>
+      <h2>Current objects in database:</h2>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import testRoutes from './routes/test.js';
+
+const app = express();
+
+app.use('/test', testRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const MONGO_CONNECTION_URL = process.env.MONGO_CONNECTION_URL
+const PORT = process.env.PORT || 3030;
+
+mongoose.set('strictQuery', false); // supress warnings
+
+mongoose.connect(MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Running on p${PORT}`)))
+    .catch((err) => console.log(err.message));
+
+
