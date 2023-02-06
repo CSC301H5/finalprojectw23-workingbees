49bc4a780ba4cbccc45ce3b1c51b936468194043
unknown <arjunrathaur@hotmail.com>
2023-02-06 18:55:37 -0500

    Created classes Hive, User for use with mongoose

---
+++ server/models/hiveModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const hiveModel = mongoose.Schema({
+    hiveID: String,
+    code: String,
+    hostID: String,
+    attendeeIDs: Array, // of strings
+    groupIDs: Array, // of strings
+    swarmIDs: Array, // of strings
+    phase: Number,
+    configOptions: String // stringify json object.
+});
+
+const HiveModel = mongoose.model('Hive', hiveModel);
+
+export default HiveModel
