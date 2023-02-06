32535f3b07f99b8ad981a7f45c0f57744cc1d87f
unknown <arjunrathaur@hotmail.com>
2023-02-06 18:55:37 -0500

    Created classes Hive, User for use with mongoose

---
+++ server/models/userModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const userModel = mongoose.Schema({
+    userID: String,
+    email: String,
+    password: String, // hashed.
+    hives: Array, // of ID's
+    profiles: Array, // profiles they have saved that they can reuse
+    isGuest: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const UserModel = mongoose.model('User', userModel);
+
+export default UserModel
