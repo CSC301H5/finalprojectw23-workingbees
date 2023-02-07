a490216796068375576e8a6a4d6a3f286d51aa05
unknown <arjunrathaur@hotmail.com>
2023-02-07 12:54:09 -0500

    Modified routing, fixed body-parser in index, wrote login/register/guestlogin endpoints

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ import apiV1routes from './routes/apiV1.js';
 
 const app = express();
 
-app.use('/test', testRoutes);
-app.use('/api/v1', apiV1routes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/test', testRoutes);
+app.use('/api/v1', apiV1routes);
 
 const MONGO_CONNECTION_URL = process.env.MONGO_CONNECTION_URL
 const PORT = process.env.PORT || 3030;
--- server/routes/apiV1.js
+++ server/routes/apiV1.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { register, login, guestLogin, getUserHives } from '../controllers/apiV1.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/guestlogin', guestLogin);
-router.get('/getUserHives', getUserHives);
+router.post('/guestRegister', guestLogin);
+router.get('/getUserHives', auth, getUserHives); // protected.
 
 export default router;
