f82443daa54be169a32649b1c4b3ef6c8c0e8b7b
Morgan Reid <kevonmorgan.reid@mail.utoronto.ca>
2023-02-09 18:29:20 -0500

    added some basic input checks

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -6,8 +6,8 @@ export default class Register extends Login{
 	//this technically means you could use the console to log in from register despite the button for it not existing
 	//unintended, but doesnt affect security
 
-	constructor(props){
-		super(props)
+	constructor(){
+		super()
 		this.state.confirmPassword = '' 
 		
 		this.handleRegister = this.handleRegister.bind(this)
@@ -15,23 +15,32 @@ export default class Register extends Login{
 	}
 	
 	handleRegister = (event) => {
-		event.preventDefault()
+		//checks input validity then sends the post request
 		
-		if(this.state.password == this.state.confirmPassword) {
+		event.preventDefault()
+		if(this.state.password != this.state.confirmPassword) {
+			this.setState({errText: "Password and confirm password must match"})
+			console.log('passwords dont match')
+		}else if(!this.checkInputs(this.state.username, this.state.password)) {
+			this.setState({errText: "Invalid email or password, passwords must be between 8-32 characters long and consist of alphanumeric characters, spaces and .,-_!@#%$"})
+		} else {
 			axios.post("/api/v1/register", {username:this.state.username, password:this.state.password}).then(res => {
 				if (res.status == 200){
 					//this auth token is stored globally and deleted at the end of the session
 					document.cookie = "x-auth-token=" + res.data.token + "; SameSite=Lax "
-					// TODO add forwarding URL
-					//window.location.replace("")	
-				} //else {}
-				// TODO: error handling
+					
+					window.location.replace("")	
+				} else if (res.status == 401) {
+					this.setState({errText: "Incorrect username or password"})
+				} else {
+					this.setState({errText: "Server error"})
+				}
 			})
-		} //else: 
+		}
     }
 	
 	//displaying 3 input fields and a button
-	render(){
+	render() {
 		return (
 		<div>
 			<h1>Welcome</h1>
@@ -61,6 +70,7 @@ export default class Register extends Login{
 				required /> <br/>
 				<input type="submit" value="Sign up" />
 			</form>
+			<p>{this.state.errText}</p>
 		</ div>
 		)
 	}
