46b4c0e24fbe33e3016ddb247435b5a3763b40f5
Morgan Reid <kevonmorgan.reid@mail.utoronto.ca>
2023-02-09 18:29:20 -0500

    added some basic input checks

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+
 export default class Login extends Component{
-	constructor(props){
-		super(props)
+	constructor(){
+		super()
 		this.state = {
 			username: '',
 			password: '',
+			errText: '',
 		}
 		
 		this.handleTextChange = this.handleTextChange.bind(this)
 		this.handleLogin = this.handleLogin.bind(this)
 
+	}
+	
+	errText = (props) => {
+		return (<div><p>{props.text}</p></div>)
+	}
+
+
+
+	
+	checkInputs = (username, password) => {
+		// checks username and password formatting
+		//emails have to follow tstandard email formatting
+		//passwords are 8-32 chars long, and alphanumeric  plus .,-_!@#%$ and spaces
+		const emailexp = /^[\w.-_]+@\w+\.\w+$/
+		const pwexp = /^[\w.,-_!@#%$ ]{8, 32}$/
+		
+		
+		return (emailexp.test(username) && pwexp.test(password) )
 	}
 	 
 	//this updates the object state if any changes happen
@@ -20,24 +40,34 @@ export default class Login extends Component{
 			this.setState({username:event.target.value});
 		} else if(event.target.name == "password") {
 			this.setState({password:event.target.value});
-		} else if (event.target.name == "confirmpassword"){
+		} else if (event.target.name == "confirmpassword") {
 			this.setState({confirmPassword:event.target.value});
 		}
 	}
 	
 	
 	handleLogin = (event) => {
-		event.preventDefault()
+		//checks inputs, then sends the request,
+		//on success saves the token as a cookie, then forwards to the logged in homepage
+		// failure at any point prints an error
 		
-        axios.post("/api/v1/login", {username:this.state.username, password:this.state.password}).then(res => {
-            if (res.status == 200){
-				//this auth token is stored globally and deleted at the end of the session
-				document.cookie = "x-auth-token=" + res.data.token + "; SameSite=Lax "
-				// TODO add forwarding URL
-				//window.location.replace("")
-			} //else {}
-			// TODO: error handling
-        })
+		event.preventDefault()
+
+		if (!this.checkInputs(this.state.username, this.state.password)){
+			this.setState({errText: "Invalid email or password, passwords must be between 8-32 characters long and consist of alphanumeric characters, spaces and .,-_!@#%$"})
+		} else {
+			axios.post("/api/v1/login", {username:this.state.username, password:this.state.password}).then(res => {
+				if (res.status == 200){
+					//this auth token is stored globally and deleted at the end of the session
+					document.cookie = "x-auth-token=" + res.data.token + "; SameSite=Lax "
+					window.location.replace("/loggedin")
+				} else if (res.status == 401) {
+					this.setState({errText: "Incorrect username or password"})
+				} else {
+					this.setState({errText: "Server error"})
+				}
+			})
+		}
     }
 	
 	//displaying 2 input fields and a button
@@ -45,7 +75,7 @@ export default class Login extends Component{
 		return (
 			<div>
 				<h1>Welcome Back</h1>
-				Sign in to return where you left off
+				<h5>Sign in to return where you left off</h5>
 				<form onSubmit={this.handleLogin}>
 					<input 
 						type="text" 
@@ -63,6 +93,7 @@ export default class Login extends Component{
 						required />
 						<input type="submit" value="Sign In" />
 				</form>
+				<p>{this.state.errText}</p>
 			</div>
 		)
 	}
