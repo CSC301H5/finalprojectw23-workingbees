06a143a7b9fceeaf207d3797d13a8fa1fec8ca35
Joshua Ng <joshuanccng@gmail.com>
2023-02-09 22:10:35 -0500

    Front end: landing page commit

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import { Link } from 'react-router-dom'
 export default class TestComponent extends Component {
     constructor() {
         super();
