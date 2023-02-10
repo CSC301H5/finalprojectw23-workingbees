b928c4135f5c21f6dd99275bf309abc180ce3598
Joshua Ng <joshuanccng@gmail.com>
2023-02-09 22:10:35 -0500

    Front end: landing page commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import Test from './components/test.js';
-import Register from './components/register.js'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from './components/Landing';
+import Login from './components/login';
+import ErrorPage from './components/Error';
+import Test from './components/test';
+//import Profile from './components/Profile';
+import Room_creation from './components/room-creation';
+import Room_Config from './components/room-config';
+import Waiting1 from './components/waiting1';
+import Register from './components/register';
 
 function App() {
   return (
-    <div>
-      <Register	/>
-    </div>
+
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+       
+        <Route path="/login" element={<Login />} />
+        <Route path="/roomConfig" element={<Room_Config />} />
+        <Route path="/waiting1" element={< Waiting1 />} />
+        <Route path="/roomCreation" element={< Room_creation />} />
+        <Route path="/register" element={< Register />} />
+        <Route path="*" element={< ErrorPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
