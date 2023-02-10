e2dce68a78888545e041fa10b898680cbdd0c8ac
Morgan Reid <kevonmorgan.reid@mail.utoronto.ca>
2023-02-09 22:35:42 -0500

    fixed a regex bug causing auth params to never send

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -28,7 +28,7 @@ export default class Login extends Component{
 		//emails have to follow tstandard email formatting
 		//passwords are 8-32 chars long, and alphanumeric  plus .,-_!@#%$ and spaces
 		const emailexp = /^[\w.-_]+@\w+\.\w+$/
-		const pwexp = /^[\w.,-_!@#%$ ]{8, 32}$/
+		const pwexp = /^[\w.,-_!@#%\$ ]{8,32}$/
 		
 		
 		return (emailexp.test(username) && pwexp.test(password) )
