b6e2062027725a4591435257a059b8d2868a108d
Katty Yu <kattyyu29@gmail.com>
2023-02-25 22:48:50 -0500

    Delete print statements in register endpoint

--- server/controllers/apiV1.js
+++ server/controllers/apiV1.js
@@ -24,8 +24,6 @@ export const register = async (req, res) => {
 
     let email = req.body.email;
     let password = req.body.password;
-    console.log(!email);
-    console.log(!password);
     // verify request (can add regex)
     if (!email || !password) {
         // error
