7f3783a558537ce779c40886666652fbcaf5308c
Katty Yu <kattyyu29@gmail.com>
2023-03-03 04:48:28 -0500

    Create multiselect component

---
+++ client/src/components/ClientMultiselect.jsx
@@ -0,0 +1,52 @@
+import "./Style.css"
+import Select from 'react-select'
+
+// takes in options, maxAllowed, required, explanation, selected, setSelected
+function ClientMultiselect(props) {
+
+    const values = props.options.map(item => ({ value: item, label: item }))
+
+    const handleChange = (e) => {
+        let arr = []
+        for (let i = 0; i < e.length; i++) {
+            arr[i] = e[i].value
+        }
+        props.setSelected(arr)
+    }
+
+    return (
+        <div>
+            <h1 className="text">{props.explanation}</h1>
+            <Select
+                className="multiselect"
+                options={values}
+                isMulti
+                styles={{
+                    control: (baseStyles, state) => ({
+                      ...baseStyles,
+                      borderColor: '#FFAF40',
+                      borderRadius: '16px',
+                      boxShadow: 'none',
+                      "&:hover": {
+                        borderColor: "#FFAF40"
+                    },
+                    }),
+                  }}
+                  theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 0,
+                    colors: {
+                    ...theme.colors,
+                      text: 'black',
+                      primary25: '#FFAF40',
+                      primary: 'black',
+                    },
+                  })}
+                onChange={handleChange}
+                // check maxAllowed
+                isOptionDisabled={() => props.selected.length >= props.maxAllowed}
+            />
+        </div>
+
+    )
+} export default ClientMultiselect
