5bd53013ce60bda10e4398f46eb3a3172c949bfc
Joshua Ng <joshuanccng@gmail.com>
2023-03-11 14:43:09 -0500

    rebase

---
+++ client/src/utils/getAuthToken.js
@@ -0,0 +1,23 @@
+
+export function getAuthToken(name) {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith('x-auth-token=')) {
+      return cookie.substring('x-auth-token='.length, cookie.length);
+    }
+  }
+  return null;
+}
+export function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
+
+export default getCookie
