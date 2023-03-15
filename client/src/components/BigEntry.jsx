780e1049021bab7aa60819877c1b62a0838a817d
JeremyNgCC <90788192+JeremyNgCC@users.noreply.github.com>
2023-03-15 18:12:28 -0400

    resolve merge conflict

--- client/src/components/BigEntry.jsx
+++ client/src/components/BigEntry.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
+const smallEntry = ({ name, pictureUrl=null }) => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', borderColor: '#FFAF40', borderBottom: "solid #FFAF40", backgroundColor:'#F6F6F6' }}>
+      {pictureUrl ? (
+        <img src={pictureUrl} alt={name} style={{width: '50px', height: '50px', borderRadius: '50%', marginRight: '20px'}} />
+      ) : (
+        <div style={{width: '50px', height: '50px', display: 'flex', borderRadius: '50%', backgroundColor: '#FFAF40', marginRight: '20px'}}></div>
+      )}
+      <p style={{display: 'inline-block', textAlign: 'center', paddingRight: '40px', fontFamily: 'Montserrat'}}>{name}</p>
+      <p style={{display: 'inline-block', textAlign: 'center', paddingRight: '20px', fontFamily: 'Montserrat'}}>{name}</p>
+
+    </div>
+  );
+}
+
+export default smallEntry;
+
 /* The description should be "Phase N" if the user is the host,
 and "Team of N" if the user is an attendee
 */
--- client/src/components/SmallEntry.jsx
+++ client/src/components/SmallEntry.jsx
@@ -1,5 +1,21 @@
-import React from "react";
-import "./Style.css"
+import React from 'react';
+
+const smallEntry = ({ name, pictureUrl=null }) => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', borderColor: '#FFAF40', borderBottom: "solid #FFAF40", backgroundColor:'#F6F6F6' }}>
+      {pictureUrl ? (
+        <img src={pictureUrl} alt={name} style={{width: '20px', height: '20px', borderRadius: '50%', marginRight: '20px'}} />
+      ) : (
+        <div style={{width: '20px', height: '20px', display: 'flex', borderRadius: '50%', backgroundColor: '#FFAF40', marginRight: '20px'}}></div>
+      )}
+      <p style={{display: 'inline-block', textAlign: 'center', paddingRight: '40px', fontFamily: 'Montserrat'}}>{name}</p>
+      <p style={{display: 'inline-block', textAlign: 'center', paddingRight: '20px', fontFamily: 'Montserrat'}}>{name}</p>
+
+    </div>
+  );
+}
+
+export default smallEntry;
 
 export default function SmallEntry({ name, children }) {
     return (
