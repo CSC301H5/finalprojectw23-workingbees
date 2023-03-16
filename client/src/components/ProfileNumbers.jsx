7a5f48b20455b5a799d216ec8172ce1d7dd3babc
Katty Yu <kattyyu29@gmail.com>
2023-03-16 17:50:01 -0400

    Pass websocket as prop from WaitingP1 to ProfileNumbers

--- client/src/components/ProfileNumbers.jsx
+++ client/src/components/ProfileNumbers.jsx
@@ -3,29 +3,22 @@ import "./Style.css"
 
 /*
 expects the following props:
-    - hiveID
-    - token: auth token 
+    - profileNums, setProfileNums
+    - socket
 */
 function ProfileNumbers(props) {
 
-    const socket = new WebSocket('ws://localhost:3030/initializeWS')
-    const [profileNums, setProfileNums] = useState(0)
-
-    socket.addEventListener('open', (event) => {
-        socket.send(JSON.stringify({ event: 'REGISTER', hiveID: String(props.hiveID), token: props.token }));
-    });
-
-    socket.addEventListener('message', (event) => {
+    props.socket.addEventListener('message', (event) => {
         //console.log('received: %s', event.data);
         let data = JSON.parse(event.data);
         if (data.event === "GROUP_PROFILE_CREATED") {
-            setProfileNums(profileNums + 1)
+            props.setProfileNums(props.profileNums + 1)
         }
     });
 
     return (
         <div>
-            <h1 className="text">{profileNums}</h1>
+            <h1 className="text">{props.profileNums}</h1>
         </div>
     )
 
--- client/src/components/Waitingp1.jsx
+++ client/src/components/Waitingp1.jsx
@@ -10,9 +10,9 @@ import ProfileNumbers from "./ProfileNumbers";
 function WaitingP1() {
   const [attendeeList, setAttendeeList] = useState([])
   const [numBees, setNumBees] = useState("0")
-  const profilesCompleted = useState("0")
   const location = useLocation();
   const navigate = useNavigate();
+  const [profileNums, setProfileNums] = useState(0)
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -25,14 +25,17 @@ function WaitingP1() {
         <img src={hives}></img>
       </ div>
       <div class="right">
-        <Navbar roomCode={location.state.code} token={location.state.token}/>
+        <Navbar roomCode={location.state.code} token={location.state.token} />
         <h2 className="roomCode">Code: </h2>
         <p className="roomCode" style={{ left: '1000px', top: '35px' }}>{location.state.code}</p>
         <form onSubmit={handleSubmit}>
           <label className="numsDescription" style={{ left: '762px' }}>bees in the hive</label>
           <label className="numsDescription" style={{ left: '1000px' }}>profiles completed</label>
-          <p className="nums" style={{ left: '1070px' }}>{profilesCompleted}</p>
-          <ProfileNumbers hiveID={location.state.hiveID} token={location.state.token}/>
+          <ProfileNumbers
+            socket={socket}
+            profileNums={profileNums}
+            setProfileNums={setProfileNums}
+          />
           <p className="nums" style={{ left: '820px' }}>{numBees}</p>
           <label className="attendees">Attendee list</label>
           <p className="attendeeList">{attendeeList}</p>
